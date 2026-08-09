ea9113c730fc9a7836016e6810804f43d5814927
Sudhangsu Shekhar Bairagi <bairagisudhangsu02@gmail.com>
2026-08-09 15:06:28 +0530

    Delete careersync-backend directory

--- careersync-backend/src/main/java/com/careersync/careersync_backend/repository/UsersRepository.java
+++
@@ -1,7 +0,0 @@
-package com.careersync.careersync_backend.repository;
-
-import com.careersync.careersync_backend.entity.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsersRepository extends JpaRepository<Users, Long> {
-}
